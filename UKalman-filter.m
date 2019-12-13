0d950511432a12fb6cc246a87657d7316507cb5e
Eugene Kim <55570572+Kim-3957@users.noreply.github.com>
2019-12-13 15:23:11 +0900

    update

--- UKalman-filter.m
+++ UKalman-filter.m
@@ -1,25 +1,14 @@
-function [result]=Ukalman_vel_HRC(Mdata,Sdata,Svel,dt,Q,R);
-Pos =      Sdata'; 
-Vel =       Svel';
-True =     Mdata';
-XEst = [Pos(1) Vel(1)]'; %% [x v]'
-PEst = eye(2);           %% Initial states
-time = dt(1,1);
-result.XPred=[XEst'];
-result.time=[time];
-result.XEst=[XEst'];
-result.XMes=[XEst'];
-result.PEst=[diag(PEst)'];
-H = [1 0];
-% UKF Parameter
-alpha=0.001;
-beta =2;
-kappa=0;
-
-n=length(XEst);%size of state vector
-lamda=alpha^2*(n+kappa)-n;
-
-%calculate weights
+function [X,P]=Ukalman_vel_realtime(X,P,Sdata,Svel,dt,Q,R);
+%%%Initialization%%%
+F = [1 dt;0 1 ];    % Constant Velocity model  
+H = [1 0];          % Observation matrix
+%%%UKF Parameter%%%
+alpha=0.001;        % Parameter for the UKF filter
+beta =2;            % Parameter for the UKF filter
+kappa=0;            % parameter for the UKF filter
+n=length(X);        %size of state vector
+lamda=alpha^2*(n+kappa)-n;          % Lamda for the sigma points
+%%%Calculate weights of the sigma points%%%
 wm=[lamda/(lamda+n)];
 wc=[(lamda/(lamda+n))+(1-alpha^2+beta)];
 for i=1:2*n
@@ -27,37 +16,29 @@ for i=1:2*n
     wc=[wc 1/(2*(n+lamda))];
 end
 gamma=sqrt(n+lamda);
-
-% Main loop
-for i =2:length(dt)
-    time = time + dt(i); 
-    F = [1 dt(i);
-         0 1 ];
-    % ------ Unscented Kalman Filter --------
-    % X Sigma Predict 
-    sigma=GenerateSigmaPoints(XEst,PEst,gamma);
-    sigma=PredictMotion(sigma,F);
-    XPred=(wm*sigma')';
-    % P Predict
-    PPred=CalcSimgaPointsCovariance(XPred,sigma,wc,Q);
-    % Update
-    XMes = [Pos(i) Vel(1)]'; % An actual measurement 
-    y = XMes - H*XPred ;       % Comparion between measurement and predicted value
-    sigma=GenerateSigmaPoints(XPred,PPred,gamma);
-    zSigma=PredictObservation(sigma,H);
-    zb=(wm*sigma')';
-    St=CalcSimgaPointsCovariance(zb,zSigma,wc,R);
-    Pxz=CalcPxz(sigma,XPred,zSigma,zb,wc);
-    K=Pxz*inv(St);
-    XEst = XPred + K*y;
-    PEst=PPred-K*St*K';
-    
-    % Simulation Result
-    result.XPred=[result.XPred;XPred'];
-    result.time=[result.time; time];
-    result.XEst=[result.XEst; XEst'];
-    result.XMes=[result.XMes; XMes'];
-    result.PEst=[result.PEst; diag(PEst)'];   
-    
-end
+%%% ---Prediction stage--- %%%
+%%% Priori Sigma Predict %%%
+sigma=GenerateSigmaPoints(XEst,PEst,gamma); % Cautious! sigma is a point not a variation
+sigma=PredictMotion(sigma,F);               % Use state estimate matrix for all of the sigma points
+%%% Priori X Sigma Predict %%%
+XPred=(wm*sigma')';                         % Expectation of sigma points are assumed as XPred 
+%%% Priori P Sigma Predict %%%
+PPred=CalcSimgaPointsCovariance(XPred,sigma,wc,Q);
+%%% ---Measurement stage--- %%%
+z = Sdata;                                  % An actual measurement 
+y = z - H*XPred ;                           % Comparion between measurement and predicted value
+%%% ---Update stage--- %%%
+%%% Post Sigma estimate %%%
+zsigma=GenerateSigmaPoints(XPred,PPred,gamma);
+zSigma=PredictObservation(zsigma,H);
+%%% Post X Sigma estimate %%%
+zb=(wm*zsigma')';
+%%% Priori P Sigma estimate %%%
+St=CalcSimgaPointsCovariance(zb,zSigma,wc,R);
+%%% Kalman gain estimate %%%
+Pxz=CalcPxz(sigma,XPred,zSigma,zb,wc);
+K=Pxz*inv(St);
+%%% Update %%%
+XEst = XPred + K*y;
+PEst=PPred-K*St*K';
 end
